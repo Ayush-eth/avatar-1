b8126617a3231b234db91cd512f59c83c39134fc
Piyush-onGIT <piyush.at.googl@gmail.com>
2022-02-01 21:13:48 +0530

    initial commit

---
+++ src/StoryReel.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Story from './Story';
+import './StoryReel.css';
+
+function StoryReel() {
+  return (
+    <div className='storyReel'>
+        <Story
+            // image="https://en.wikipedia.org/wiki/Iron_Man_(2008_film)#/media/File:Iron_Man_(2008_film)_poster.jpg"
+            // profileSrc="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
+            // title="User1"
+            image=""
+            profileSrc=""
+            title=""
+        />
+
+        <Story
+            // image="https://en.wikipedia.org/wiki/Captain_America:_The_First_Avenger#/media/File:Captain_America_The_First_Avenger_poster.jpg"
+            // profileSrc="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
+            // title="User2"
+            image=""
+            profileSrc=""
+            title=""
+        />
+
+        <Story
+            // image="https://en.wikipedia.org/wiki/Thor:_Ragnarok#/media/File:Thor_Ragnarok_poster.jpg"
+            // profileSrc="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
+            // title="User3"
+            image=""
+            profileSrc=""
+            title=""
+        />
+
+        <Story
+            // image="https://en.wikipedia.org/wiki/The_Incredible_Hulk_(film)#/media/File:The_Incredible_Hulk_(film)_poster.jpg"
+            // profileSrc="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
+            // title="User4"
+            image=""
+            profileSrc=""
+            title=""
+        />
+
+    </div>
+    );
+}
+
+export default StoryReel;
